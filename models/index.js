382351b77831b2e5b4609faa74a3783812230943
amisha-11 <75255367+amisha-11@users.noreply.github.com>
2022-06-26 16:02:28 +0530

    Delete models directory

--- models/index.js
+++
@@ -1,8 +0,0 @@
-const dbConfig = require("../config/db.config.js");
-const mongoose = require("mongoose");
-mongoose.Promise = global.Promise;
-const db = {};
-db.mongoose = mongoose;
-db.url = dbConfig.url;
-db.hacks = require("./hack.model.js")(mongoose);
-module.exports = db;
